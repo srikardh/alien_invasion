a7b9fc178a9fefd4ab2d63317b427651898e35d3
Srikar <sri.dharmavaram@gmail.com>
2026-01-01 17:19:38 -0500

    Adding BG color

--- alieninvasion.py
+++ alieninvasion.py
@@ -7,12 +7,14 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
     
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
 
 
